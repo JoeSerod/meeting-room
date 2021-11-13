428c0fccafb1c357539c8ea33babf57e279a22ec
Camilo Segovia <54286208+JoeSerod@users.noreply.github.com>
2021-11-13 16:02:20 -0600

    schedule component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ScheduleComponent } from './components/schedule/schedule.component';
+import { ReservationFormComponent } from './components/reservation-form/reservation-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScheduleComponent,
+    ReservationFormComponent
   ],
   imports: [
     BrowserModule,
