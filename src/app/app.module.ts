00e93e4438118ddc437e807db02041ec4ec300dc
Camilo Segovia <54286208+JoeSerod@users.noreply.github.com>
2021-11-13 16:22:13 -0600

    app header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { ScheduleComponent } from './components/schedule/schedule.component';
 import { ReservationFormComponent } from './components/reservation-form/reservation-form.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
