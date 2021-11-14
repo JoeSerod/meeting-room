55308540bd77fe0364c0f8b575aeacfb4e11f1d2
Camilo Segovia <54286208+JoeSerod@users.noreply.github.com>
2021-11-14 13:25:25 -0600

    open dialog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ReservationFormComponent } from './components/reservation-form/reservation-form.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'meeting-room';
+  date : Date;
+  name: String;
+  constructor(
+    public dialog: MatDialog
+  ){
+
+  }
+  ngOnInit(){
+
+  }
+  openFormDialog():void{
+    console.log("dialog");
+    const dialogRef =  this.dialog.open(ReservationFormComponent,{
+      width:"250px",
+      data: {date: this.date, name: this.name}
+    })
+  }
 }
--- src/app/components/reservation-form/reservation-form.component.ts
+++ src/app/components/reservation-form/reservation-form.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
-  selector: 'app-reservation-form',
+  selector: 'reservation-form',
   templateUrl: './reservation-form.component.html',
   styleUrls: ['./reservation-form.component.less']
 })
 export class ReservationFormComponent implements OnInit {
-
-  constructor() { }
+  date: Date;
+  name: String;
+  constructor(
+    public dialog: MatDialog
+  ) { }
 
   ngOnInit(): void {
   }
-
+  openFormDialog():void{
+    const dialogRef = this.dialog.open(ReservationFormComponent,{
+      width:"250px",
+      data:{date: this.date, name:this.name}
+    });
+    dialogRef.afterClosed().subscribe(result=>{
+      console.log("closed");
+      
+    })
+    
+  }
 }
