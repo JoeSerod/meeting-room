c7163bbbf3f038b09c5320ef6f3e7fc7a78e8e0a
Camilo Segovia <54286208+JoeSerod@users.noreply.github.com>
2021-11-14 17:27:41 -0600

    get end date

--- src/app/components/reservation-form/reservation-form.component.ts
+++ src/app/components/reservation-form/reservation-form.component.ts
@@ -10,6 +10,8 @@ import { SelectDateService } from 'src/app/services/selectDate.service';
 export class ReservationFormComponent implements OnInit {
   date: Date;
   name: String;
+  startDate: Date;
+  endDate: Date;
   constructor(
     public dialog: MatDialog,
     private selectDateService :SelectDateService
@@ -32,13 +34,20 @@ export class ReservationFormComponent implements OnInit {
     })
     
   }
-  onChangeStartTime(e){
+  onChangeStartTime(e, type){
     let timeExp = e.match(/(\d+)\:(\d+) (\w+)/),
     hours = timeExp[3]==="AM"? timeExp[1]==="12"? 0 :timeExp[1]: timeExp[1]==="12"?12 :+timeExp[1]+12,
     minutes = timeExp[2];
-    this.date.setHours(hours, minutes)
+ 
     
-    console.log(this.date);
+    let time = new Date(this.date.getFullYear(), this.date.getMonth(), this.date.getDate(), hours, minutes)
+    if (type === "start") {
+      this.startDate = time;
+    }else{
+      this.endDate = time;
+    }
+    console.log(this.startDate);
+    console.log(this.endDate);
     
    
    
