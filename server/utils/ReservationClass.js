790a9d2884a5150f57685473ff7ab676dd8494a3
Camilo Segovia <54286208+JoeSerod@users.noreply.github.com>
2021-11-15 15:29:23 -0600

    register if available

--- server/utils/ReservationClass.js
+++ server/utils/ReservationClass.js
@@ -8,10 +8,11 @@ class ReservationClass{
      * 
      * @param {Date} startDate 
      * @param {Date} endDate 
+     * @return {Boolean} false if the room availabilty is false and true if it's available
      */
     async verifyAvailability (startDate, endDate, room) {
         console.log(startDate, endDate);
-        ReservationModel.find({
+        return ReservationModel.find({
          $and:[
              { $or:[
                 {$and:[
@@ -29,29 +30,41 @@ class ReservationClass{
          ]
         }).then((value)=>{
             console.log(value);
+            if(value.length>0){
+                return false;
+            }
+            return true;
+            
+        }).catch((error)=>{
+            console.log(error);
+            return false;
         })
     }
     /**
      * 
      * @param {Object} model 
      */
-    saveSchedule(params){
+   async saveSchedule(params){
         const model = new ReservationModel();
         model.name = params.name;
         model.startDate = params.startDate;
         model.endDate = params.endDate;
         model.room = params.room;
-        this.verifyAvailability(model.startDate, model.endDate, model.room);
-        // const response = model.save().then((docStored)=>{
-        //     if(!docStored) return{status: 404, body:{message:"the room has not been reserved"}}
-        //     return {status:200, body:{doc: docStored, message:"success"}};
-        // }).catch((err)=>{
-         
-        //     return {status: 500,body:{message:err}};
-        // })
+        const available = await this.verifyAvailability(model.startDate, model.endDate, model.room);
+        if(available){
+            const response = model.save().then((docStored)=>{
+            if(!docStored) return{status: 404, body:{message:"the room has not been reserved"}}
+            return {status:200, body:{doc: docStored, message:"success"}};
+            }).catch((err)=>{
+            
+                return {status: 500,body:{message:err}};
+            })
+            
+            return response;
+        } 
+        return {status:200, body:{message:" The room is not available"}};
         
-        // return response;
-        return {"merr": "OK"}
+     
     }
     
 }
