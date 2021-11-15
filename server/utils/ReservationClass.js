1e33612f78d8a047271754632c108b59f7957732
Camilo Segovia <54286208+JoeSerod@users.noreply.github.com>
2021-11-15 15:21:36 -0600

    verify room availability

--- server/utils/ReservationClass.js
+++ server/utils/ReservationClass.js
@@ -9,18 +9,24 @@ class ReservationClass{
      * @param {Date} startDate 
      * @param {Date} endDate 
      */
-    async verifyAvailability (startDate, endDate) {
+    async verifyAvailability (startDate, endDate, room) {
         console.log(startDate, endDate);
         ReservationModel.find({
-          $or:[
-            {$and:[
-                {"startDate":{$lt: startDate}},
-                {"endDate":{$gte: startDate}}
-            ]},{ $and:[
-                {"startDate":{$lt: endDate}},
-                {"endDate":{ $gte: endDate}},
-            ]}
-          ]
+         $and:[
+             { $or:[
+                {$and:[
+                    {"startDate":{$lt: startDate}},
+                    {"endDate":{$gte: startDate}}
+                ]},{ $and:[
+                    {"startDate":{$lt: endDate}},
+                    {"endDate":{ $gte: endDate}},
+                ]}
+              ]},
+              {
+                  "room": {$eq: room}
+              }
+            
+         ]
         }).then((value)=>{
             console.log(value);
         })
@@ -35,7 +41,7 @@ class ReservationClass{
         model.startDate = params.startDate;
         model.endDate = params.endDate;
         model.room = params.room;
-        this.verifyAvailability(model.startDate, model.endDate);
+        this.verifyAvailability(model.startDate, model.endDate, model.room);
         // const response = model.save().then((docStored)=>{
         //     if(!docStored) return{status: 404, body:{message:"the room has not been reserved"}}
         //     return {status:200, body:{doc: docStored, message:"success"}};
