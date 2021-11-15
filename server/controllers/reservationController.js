37081d5f3c3b500e47d35b2a07e377ccf141fd0e
Camilo Segovia <54286208+JoeSerod@users.noreply.github.com>
2021-11-15 11:17:32 -0600

    save doc in db

--- server/controllers/reservationController.js
+++ server/controllers/reservationController.js
@@ -1,4 +1,6 @@
 'use strict'
+const ReservationModel = require("../models/reservationModel");
+
 
 const controller = {
     getSchedule: function (req, res) {
@@ -10,10 +12,35 @@ const controller = {
             
         }
     },
+    /**
+     * 
+     * @param {Request} req 
+     * @param {Response} res 
+     * @returns 
+     */
     saveSchedule: function (req, res) {
-        return res.status(200).send({
-            message: "save schedule route OK"
-        })
+       try {
+           const model = new ReservationModel();
+           const params = req.body;
+           model.name = params.name;
+           model.startDate = params.startDate;
+           model.endDate = params.endDate;
+           model.room = params.room;
+           model.save((err, docStored)=>{
+               console.log(err, docStored);
+               if(err) return res.status(500).send({message:"error while saving data"});
+               if(!docStored) return res.status(404).send({message:"the room has not been reserved"})
+
+               return res.status(200).send({doc: docStored, message:"success"});
+           })
+//            const [name, startDate, endDate, room] = params;
+// console.log(name);
+        // return res.status(200).send({
+        //     message: params
+        // })
+       } catch (error) {
+           
+       }
     }
 }
 
