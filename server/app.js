0b7ffc0a01f9f55280095d7ec0bb60cf244953f3
Camilo Segovia <54286208+JoeSerod@users.noreply.github.com>
2021-11-15 10:55:05 -0600

    body parser added

--- server/app.js
+++ server/app.js
@@ -7,6 +7,7 @@ const routes = require("./routes/reservationRoutes");
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 app.use("/", routes)
 
 module.exports= app;
