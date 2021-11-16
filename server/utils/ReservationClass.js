f68d5497cfb2736c2185488c57db31d1a6dc6e41
Camilo Segovia <54286208+JoeSerod@users.noreply.github.com>
2021-11-16 12:44:55 -0600

    verify if date has not expired

--- server/utils/ReservationClass.js
+++ server/utils/ReservationClass.js
@@ -45,24 +45,32 @@ class ReservationClass{
      * @param {Object} model 
      */
    async saveSchedule(params){
-        const model = new ReservationModel();
-        model.name = params.name;
-        model.startDate = params.startDate;
-        model.endDate = params.endDate;
-        model.room = params.room;
-        const available = await this.verifyAvailability(model.startDate, model.endDate, model.room);
-        if(available){
-            const response = model.save().then((docStored)=>{
-            if(!docStored) return{status: 404, body:{message:"the room has not been reserved"}}
-            return {status:200, body:{doc: docStored, message:"success"}};
-            }).catch((err)=>{
-            
-                return {status: 500,body:{message:err}};
-            })
-            
-            return response;
-        } 
-        return {status:200, body:{message:" The room is not available"}};
+       const todayDate= new Date();
+       const toDate=new Date(params.startDate);
+      
+       if (toDate.getTime()>=todayDate.getTime()) {
+            const model = new ReservationModel();
+            model.name = params.name;
+            model.startDate = params.startDate;
+            model.endDate = params.endDate;
+            model.room = params.room;
+            const available = await this.verifyAvailability(model.startDate, model.endDate, model.room);
+            if(available){
+                const response = model.save().then((docStored)=>{
+                if(!docStored) return{status: 404, body:{message:"the room has not been reserved"}}
+                return {status:200, body:{doc: docStored, message:"success"}};
+                }).catch((err)=>{
+                
+                    return {status: 500,body:{message:err}};
+                })
+                
+                return response;
+            } 
+            return {status:200, body:{message:" The room is not available"}};
+       }
+       
+       return {status:200, body:{message:" The date has been expired"}};
+       
         
      
     }
